30bd35d968775788ecf02a57123848921ccb5b23
Yurii Rashkovskii <yrashk@gmail.com>
2012-09-29 21:53:00 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,7 @@
+Expm.Package.new(name: "mimetypes", description: "Erlang MIME types library",
+                 version: :head, keywords: ["Erlang","mime","type","file","extension"],
+                 maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
+                 contributors: [[name: "Magnus Klaar", email: "magnus.klaar@gmail.com"],
+                                [name: "Uvarov Michael", email: "freeakk@gmail.com"],
+                                [name: "Jesse Gumm", email: "sigmastar@gmail.com"]],
+                 repositories: [[github: "yrashk/mimetypes"]])
