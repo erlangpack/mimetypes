0800544bbb684616651a2a1b30aa260132d17d37
Yurii Rashkovskii <yrashk@gmail.com>
2013-01-02 06:39:34 -0800

    Update expm package

--- package.exs
+++ package.exs
@@ -1,7 +1,7 @@
 Expm.Package.new(name: "mimetypes", description: "Erlang MIME types library",
-                 version: "0.9", keywords: ["Erlang","mime","type","file","extension"],
+                 version: "1.0", keywords: ["Erlang","mime","type","file","extension"],
                  maintainers: [[name: "Yurii Rashkovskii", email: "yrashk@gmail.com"]],
                  contributors: [[name: "Magnus Klaar", email: "magnus.klaar@gmail.com"],
                                 [name: "Uvarov Michael", email: "freeakk@gmail.com"],
                                 [name: "Jesse Gumm", email: "sigmastar@gmail.com"]],
-                 repositories: [[github: "spawngrid/mimetypes", tag: "0.9"]])
+                 repositories: [[github: "spawngrid/mimetypes", tag: "1.0"]])
